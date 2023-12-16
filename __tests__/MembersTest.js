94fa2f0dbb1cc6b267c624f0564859380af616bc
Parkhanyoung <phanyoung@naver.com>
2023-12-16 17:53:19 +0900

    test: 멤버 클래스에 대한 테스트 작성

---
+++ __tests__/MembersTest.js
@@ -0,0 +1,12 @@
+import Member from "../src/model/member.js";
+
+describe("멤버에 대한 테스트", () => {
+  describe("이름이 1자 이상 5자 이하가 아닐 경우 예외처리한다.", () => {
+    test.each([
+      [() => new Member(""), ""],
+      [() => new Member("우아한테크코스"), "우아한테크코스"],
+    ])(`%s 입력 이름: %s`, (generateMember, name) => {
+      expect(() => generateMember()).toThrow("[ERROR]");
+    });
+  });
+});
