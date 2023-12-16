75ae6dd76ff64551803ed9d8be47dcef6059fe4c
Parkhanyoung <phanyoung@naver.com>
2023-12-16 17:57:54 +0900

    test: members 클래스에 대한 테스트 작성

--- __tests__/MembersTest.js
+++ __tests__/MembersTest.js
@@ -1,12 +1,16 @@
 import Member from "../src/model/member.js";
+import Members from "../src/model/members.js";
 
-describe("멤버에 대한 테스트", () => {
-  describe("이름이 1자 이상 5자 이하가 아닐 경우 예외처리한다.", () => {
+describe("멤버 콜렉션에 대한 테스트", () => {
+  describe("멤버 수가 5명 미만, 35명 초과일 경우 예외처리한다.", () => {
+    const tooLittleMembers = new Array(4).fill().map((_, index) => new Member(`${index}`));
+    const tooManyMembers = new Array(36).fill().map((_, index) => new Member(`${index}`));
+
     test.each([
-      [() => new Member(""), ""],
-      [() => new Member("우아한테크코스"), "우아한테크코스"],
-    ])(`%s 입력 이름: %s`, (generateMember, name) => {
-      expect(() => generateMember()).toThrow("[ERROR]");
+      [() => new Members(tooLittleMembers), tooLittleMembers.length],
+      [() => new Members(tooManyMembers), tooManyMembers.length],
+    ])(`%s 입력 멤버 수: %s`, (generateMembers, count) => {
+      expect(() => generateMembers()).toThrow("[ERROR]");
     });
   });
 });
